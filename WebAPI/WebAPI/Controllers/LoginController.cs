dbec8607aafd3703e0eea92e323dfdf278d59e6f
Wesley van Loon <KingTamotsu@outlook.com>
2018-11-19 20:53:58 +0100

    Initialize WebApi

---
+++ WebAPI/WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class LoginController : ApiController
+    {
+        //TODO Add read,write,put and delete methods.
+
+    }
+}
